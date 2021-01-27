5e48b14166137063cedd1d12baeca63bfaf866f7
KuicsCristian <kuics.cristian@gmail.com>
2021-01-27 16:50:09 +0200

    3

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,17 +1,18 @@
+
 export const homeObj1 = {
     id: 'about',
     lightBg: false,
     lightText: true,
     lightTextDesc: true,
-    TopLine: 'Premium Experiences',
+    topLine: 'Premium Experiences',
     headline: 'Countless stars to be seen',
     description: 'Come and get your own piece of cosmic wonders!',
     buttonLabel: 'Get Started',
     imgStart: false,
-    img: require ('../../images/svg-1.svg'),
+    img: require('../../images/svg-1.svg'),   
     alt: 'Moon',
     dark: 'true',
     primary:'true',
-    darkText: false
-
+    darkText: false,
+   
 }
